c9c5ecac2b48c4682637383eeeb41bd245d11e1c
chri911 <delightsome@bigmir.net>
2021-03-04 23:06:12 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.6ed0a18b093444654ed3e2b792e6f1ab.js"
 );
 
 self.addEventListener('message', (event) => {
